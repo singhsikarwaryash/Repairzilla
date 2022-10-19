387c6a8d1f7074d243a84fb89900466c0324dc26
singhsikarwaryash <singhsikarwaryash@gmail.com>
2022-10-20 02:28:20 +0530

    deployment

--- repair/settings.py
+++ repair/settings.py
@@ -133,6 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
