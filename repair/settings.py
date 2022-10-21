a3bd9b231abc4831aed000115b4020019b1caab4
singhsikarwaryash <singhsikarwaryash@gmail.com>
2022-10-21 21:11:10 +0530

    deployment

--- repair/settings.py
+++ repair/settings.py
@@ -140,9 +140,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-# STATIC_ROOT =[
-#  os.path.join(BASE_DIR,'staticfiles')
-# ]
+STATIC_ROOT =[
+ os.path.join(BASE_DIR,'staticfiles')
+]
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
